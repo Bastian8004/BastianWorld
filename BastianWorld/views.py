20cd3e76d58a93f4d9f49aca81cc4ba32f258854
Bastian8004 <123553744+Bastian8004@users.noreply.github.com>
2024-08-21 17:43:13 +0200

    Update views.py

--- BastianWorld/views.py
+++ BastianWorld/views.py
@@ -23,18 +23,16 @@ def gallery(request):
     try:
         albums = paginator.page(page)
     except PageNotAnInteger:
-        albums = paginator.page(1) # If page is not an integer, deliver first page.
+        albums = paginator.page(1) 
     except EmptyPage:
-        albums = paginator.page(paginator.num_pages) # If page is out of range (e.g.  9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
 
     return render(request, 'blog.html', { 'albums': list })
 
 class AlbumDetail(DetailView):
      model = Album
      def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super(AlbumDetail, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the images
         context['images'] = AlbumImage.objects.filter(album=self.object.id)
         return context
 
